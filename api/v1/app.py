89df4e1a551aa7aeea0fa10aa2284c0fd78173ca
root <luyolo.tuta23@gmail.com>
2024-05-27 10:13:06 -0700

    check again

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
-from os import getenv
-from flask import Flask
+"""
+Initialize Flask Application
+"""
+import os
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
-if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """closes current session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """handles 404 errors by returning a JSON formatted response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-"""
-Create index.py to connect to API
-"""
+"""Define routes"""
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status')
-def api_status():
-    """
-    API status
-    """
-    response = ('status': "OK")
-    return jsonify(response)
+@app_views.route('/status', methods=['GET'])
+def status():
+    """returns status JSON message for route"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each object by type"""
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(stats)
