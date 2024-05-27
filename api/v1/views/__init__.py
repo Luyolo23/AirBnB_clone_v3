8813097fef0b31e0c7fc2e974879efc130dcc4d7
root <luyolo.tuta23@gmail.com>
2024-05-27 03:02:28 -0700

    Status of your API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+creating a Flask app blueprint
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1') 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""
+Create index.py to connect to API
+"""
+from flask import jsonify
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
-from models import storage
 
+@app_views.route('/status')
+def api_status():
+    """
+    API status
+    """
+    response = ('status': "OK")
+    return jsonify(response)
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
-
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass)
